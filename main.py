c6cfd674330f631c669730029871328a0e1bb212
Killyan Povoa <kpovoa@numericable.fr>
2022-11-07 17:21:16 +0100

    first commit in flask branch

---
+++ main.py
@@ -0,0 +1,43 @@
+# import os
+import random
+import datetime
+from google.cloud import datastore, storage
+import google.oauth2.id_token
+from flask import Flask, render_template, request, redirect, url_for
+from google.auth.transport import requests
+
+# credential_path = "h:\Desktop\Cloud Programming\Labs\env/assignment01-343611-d88e3f82d83b.json"
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+app = Flask(__name__)
+datastore_client = datastore.Client()
+firebase_request_adapter = requests.Request()
+
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+@app.route('/connection')
+def connection():
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+    user = None
+
+    if id_token:
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(id_token, firebase_request_adapter)
+            user = retrieveUser(claims)
+            if user == None:
+                return render_template('firstConnection.html', message="")
+            else:
+                return redirect('/index')
+        except ValueError as exc:
+            error_message = str(exc)
+    return redirect('/')
+
+
+
