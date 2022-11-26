0a94c7e339f636b42730fddd7931e275a06dc484
KP1806 <kpovoa@numericable.fr>
2022-11-26 12:00:34 +0100

    Ajout du code de Max

--- main.py
+++ main.py
@@ -25,23 +25,22 @@ auth = False
 bdd_tsr = {}
 bdd_tsq = {}
 
-nb_tsq = os.popen("ls -l tsq_files/ | wc -l").read()
-nb_tsr = os.popen("ls -l tsr_files/ | wc -l").read()
-tsr = os.popen("ls tsr_files/").read()
-tsq = os.popen("ls tsq_files/").read()
-lst_tsr = tsr.split("\n")
-lst_tsq = tsq.split("\n")
-lst_tsr = lst_tsr[:len(lst_tsr) -1]
-lst_tsq = lst_tsq[:len(lst_tsq) -1]
+def initializeBdd():
+    tsr = os.popen("ls timestamp_files/tsr_files/").read()
+    tsq = os.popen("ls timestamp_files/tsq_files/").read()
+    lst_tsr = tsr.split("\n")
+    lst_tsq = tsq.split("\n")
+    lst_tsr = lst_tsr[:len(lst_tsr) -1]
+    lst_tsq = lst_tsq[:len(lst_tsq) -1]
 
-for tsr_file in lst_tsr:
-    with open("tsr_files/" + tsr_file, "rb") as timestamp_file:
-        encoded_string = base64.b64encode(timestamp_file.read())
-    final_string = str(encoded_string)
-    bdd_tsr[final_string] = tsr_file
-    bdd_tsq[final_string] = lst_tsq[lst_tsr.index(tsr_file)]
+    for tsr_file in lst_tsr:
+        with open("timestamp_files/tsr_files/" + tsr_file, "rb") as timestamp_file:
+            encoded_string = base64.b64encode(timestamp_file.read())
+        final_string = str(encoded_string)
+        bdd_tsr[final_string] = tsr_file
+        bdd_tsq[final_string] = lst_tsq[lst_tsr.index(tsr_file)]
 
-
+initializeBdd()
 
 @app.route('/')
 def root():
@@ -117,10 +116,10 @@ def getTimestamp(img, nom, prenom, intitule):
     timestamp_file_name = txt + ".tsr"
     os.system("openssl ts -query -data " + img.filename + " -no_nonce -sha512 -cert -out " + request_file)
     os.system("curl -H \"Content-Type: application/timestamp-query\" --data-binary '@" + request_file + "' https://freetsa.org/tsr > " + timestamp_file_name)
-    os.system("mv " + request_file + " tsq_files/")
-    os.system("mv " + timestamp_file_name + " tsr_files/")
+    os.system("mv " + request_file + " timestamp_files/tsq_files/")
+    os.system("mv " + timestamp_file_name + " timestamp_files/tsr_files/")
 
-    with open("tsr_files/" + timestamp_file_name, "rb") as timestamp_file:
+    with open("timestamp_files/tsr_files/" + timestamp_file_name, "rb") as timestamp_file:
         encoded_string = base64.b64encode(timestamp_file.read())
     final_string = str(encoded_string)
     bdd_tsq[final_string] = request_file
@@ -169,7 +168,7 @@ def verifPage():
 def verifTimestamp(encoded):
     timestamp_file = bdd_tsr[encoded]
     request_file = bdd_tsq[encoded]
-    verif = os.popen("openssl ts -verify -in tsr_files/" + timestamp_file + " -queryfile tsq_files/" + request_file + " -CAfile cacert.pem -untrusted tsa.crt").read()
+    verif = os.popen("openssl ts -verify -in timestamp_files/tsr_files/" + timestamp_file + " -queryfile timestamp_files/tsq_files/" + request_file + " -CAfile timestamp_files/cacert.pem -untrusted timestamp_files/tsa.crt").read()
     return verif 
 
 
